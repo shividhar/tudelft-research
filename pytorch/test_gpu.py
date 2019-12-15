b46494311cd382218fae0d1a7f9f62364b3c6a74
Shivansh Dhar <shivanshdhar@gmail.com>
2019-12-15 19:30:22 +0100

    more testing

--- pytorch/test_gpu.py
+++ pytorch/test_gpu.py
@@ -1,5 +1,8 @@
 import torch
-print(torch._C._cuda_getDriverVersion())
+print(torch.cuda.current_device())
+print(torch.cuda.device(0))
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
 print(torch.cuda.is_available())
+print(torch._C._cuda_getDriverVersion())
 print(torch.cuda.nccl.version())
-torch.cuda.current_device()
